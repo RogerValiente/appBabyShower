618595732d4890d1d0a633dec59b755f4f3df20f
RogerValiente <64560821+RogerValiente@users.noreply.github.com>
2023-10-05 22:55:35 -0500

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,7 +7,7 @@ export const Footer = () => {
       <Container>
         <Row className="align-items-center">
           <Col size={12} sm={6} className="mt-5">
-            <img src={logo} alt="Logo" />
+            <img style={{ width: '30%', position: 'relative', bottom: '0px' }} src={logo} alt="Logo" />
           </Col>
           <Col size={12} sm={6} className="text-center text-sm-end" style={{ marginTop: '40px' }}>
             <p>Copyright 2023. All Rights Reserved</p>
